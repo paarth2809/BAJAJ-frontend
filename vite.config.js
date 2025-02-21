dd769d7034dfe4a887c2acd01b8a4d95ea13ed1b
Paarth Monga <paarthmonga28@gmail.com>
2025-02-21 17:21:29 +0530

    frontend

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: true, // Fix for Render
+    port: 3000, // Ensure it runs on a valid port
+  },
+});
