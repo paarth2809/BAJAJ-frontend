fa74edbc0b8157a9d1e4384f32478e8455cdd46b
Paarth Monga <paarthmonga28@gmail.com>
2025-02-21 17:30:37 +0530

    frontend

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Allow access from external networks (important for Render)
+    host: 'https://bajaj-ycbu.onrender.com/bfhl', // Allow access from external networks (important for Render)
     port: 3000, // Ensure Vite runs on a known port
     strictPort: true, // Enforce the exact port
     allowedHosts: ['bajaj-frontend-ei5o.onrender.com'], // Add your Render domain
